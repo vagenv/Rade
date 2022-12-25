a22d093aa9ed57dfbb18dde02f1d916167f32818
Vagen <vagenv@gmail.com>
2022-12-25 14:15:10 -0800

    Added RJetpackComponent.

---
+++ Source/Rade/RJetpackComponent.cpp
@@ -0,0 +1,54 @@
+// Copyright 2015-2023 Vagen Ayrapetyan
+
+#include "RJetpackComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "GameFramework/Character.h"
+#include "TimerManager.h"
+
+URJetpackComponent::URJetpackComponent (const class FObjectInitializer& PCIP)
+{
+   CurrentChargePercent = 50;
+   MinUseablePercent    = 40;
+   RestoreSpeed         = 0.05f;
+   RestorePower         = 0.5f;
+   PushPower            = 7;
+}
+
+void URJetpackComponent::BeginPlay()
+{
+   Super::BeginPlay ();
+
+   ACharacter *Character = Cast<ACharacter> (GetOwner ());
+   if (Character) {
+      MovementComponent = Character->GetCharacterMovement();
+
+      FTimerHandle FillUpHandle;
+      Character->GetWorldTimerManager().SetTimer (FillUpHandle, this, &URJetpackComponent::FillUp, RestoreSpeed, true);
+   }
+}
+
+void URJetpackComponent::Use_Implementation ()
+{
+   if (!MovementComponent) return;
+   if (MovementComponent->IsMovingOnGround ()) return;
+
+   if (CurrentChargePercent < MinUseablePercent) return;
+
+   // Apply power
+   MovementComponent->Velocity.Z += CurrentChargePercent * PushPower;
+   CurrentChargePercent = 0;
+}
+
+void URJetpackComponent::FillUp ()
+{
+   if (!MovementComponent) return;
+
+   if (MovementComponent->IsMovingOnGround ()) {
+      if (CurrentChargePercent < 100) {
+         CurrentChargePercent += RestorePower;
+         if (CurrentChargePercent > 100) {
+            CurrentChargePercent = 100;
+         }
+      }
+   }
+}
---
+++ Source/Rade/RJetpackComponent.h
@@ -0,0 +1,58 @@
+// Copyright 2015-2023 Vagen Ayrapetyan
+#pragma once
+
+#include "Components/ActorComponent.h"
+#include "RJetpackComponent.generated.h"
+
+UCLASS()
+class RADE_API URJetpackComponent : public UActorComponent
+{
+   GENERATED_BODY()
+public:
+
+   //--------------------------------------------------------------------------
+   //                         Core
+   //--------------------------------------------------------------------------
+
+   // Base events
+   URJetpackComponent (const class FObjectInitializer& PCIP);
+   virtual void BeginPlay () override;
+
+   // Owners Movement Component
+   class UCharacterMovementComponent *MovementComponent;
+
+   //--------------------------------------------------------------------------
+   //                         Params
+   //--------------------------------------------------------------------------
+
+   // Current Charge Percent
+   UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Rade|Jetpack")
+      float CurrentChargePercent;
+
+   // Minimal Usable Charge
+   UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Rade|Jetpack")
+      float MinUseablePercent;
+
+   // restore Speed
+   UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Rade|Jetpack")
+      float RestoreSpeed;
+
+   // Restore Value
+   UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Rade|Jetpack")
+      float RestorePower;
+
+   // Push Multiplier
+   UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Rade|Jetpack")
+      float PushPower;
+
+   //--------------------------------------------------------------------------
+   //                         Calls
+   //--------------------------------------------------------------------------
+
+   UFUNCTION(Reliable, Server)
+      void Use ();
+      void Use_Implementation ();
+
+   protected:
+      void FillUp ();
+};
