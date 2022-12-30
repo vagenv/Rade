56f98589dad7711fedfd997f30ba14daf91cb112
Vagen <vagenv@gmail.com>
2022-12-29 19:41:20 -0800

    Standalone batch script updated.

--- standalone.bat
+++ standalone.bat
@@ -2,4 +2,4 @@
 
 call %~dp0\var.bat
 
-call %BUILD_BAT% %PROJECT% Win64 Development "%UPROJECT_PATH%" -waitmutex -NoHotReload
+start "" %BUILD_BAT% %PROJECT% Win64 Development "%UPROJECT_PATH%" -waitmutex -NoHotReload
