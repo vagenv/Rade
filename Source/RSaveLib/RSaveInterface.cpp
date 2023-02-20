2b3afdc7ca936fff7741ebc7d323e7ea80fdace4
Vagen <vagenv@gmail.com>
2023-02-19 23:02:08 -0800

    RSaveLib. Added RSaveInterface. Sketch of much simpler save/load interface.

---
+++ Source/RSaveLib/RSaveInterface.cpp
@@ -0,0 +1,41 @@
+// Copyright 2015-2023 Vagen Ayrapetyan
+
+#include "RSaveInterface.h"
+#include "RSaveMgr.h"
+#include  "RUtilLib/RLog.h"
+
+void IRSaveInterface::Init_Save (UWorld *World_, const FString &SaveId)
+{
+   ObjectSaveId = SaveId;
+   World = World_;
+
+   FRSaveEvent SavedDelegate;
+   SavedDelegate.AddDynamic (this, &IRSaveInterface::OnSave_Internal);
+   URSaveMgr::OnSave (World, SavedDelegate);
+
+   FRSaveEvent LoadedDelegate;
+   LoadedDelegate.AddDynamic (this, &IRSaveInterface::OnLoad_Internal);
+   URSaveMgr::OnLoad (World, LoadedDelegate);
+}
+
+void IRSaveInterface::OnSave_Internal ()
+{
+   FBufferArchive ToBinary;
+
+   OnSave (ToBinary);
+
+   // Set binary data to save file
+   if (!ensure (URSaveMgr::Set (World, ObjectSaveId, ToBinary))) return;
+}
+
+void IRSaveInterface::OnLoad_Internal ()
+{
+   // Get binary data from save file
+   TArray<uint8> BinaryArray;
+   if (!ensure (URSaveMgr::Get (World, ObjectSaveId, BinaryArray))) return;
+
+   FMemoryReader FromBinary = FMemoryReader (BinaryArray, true);
+   FromBinary.Seek(0);
+
+   OnLoad (FromBinary);
+}
