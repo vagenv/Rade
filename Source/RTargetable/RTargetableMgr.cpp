728692a8cfe2d230a7fb722734b670506307be78
Vagen <vagenv@gmail.com>
2023-03-08 14:41:22 -0800

    RTargetableMgr implemented seach with angle and distance.

--- Source/RTargetable/RTargetableMgr.cpp
+++ Source/RTargetable/RTargetableMgr.cpp
@@ -43,3 +43,43 @@ void URTargetableMgr::RmTarget  (URTargetableComponent * Target)
    TargetableList.Remove (Target);
    OnListUpdated.Broadcast ();
 }
+
+URTargetableComponent* URTargetableMgr::Find (FVector Origin, FRotator Direction, TArray<AActor*> FilterOut)
+{
+   // Camera look direction
+   FVector SearchDir = Direction.Vector ();
+   SearchDir.Normalize ();
+   URTargetableComponent* CurrentTarget = nullptr;
+
+   // --- Iterate over Targets
+   for (URTargetableComponent* ItTarget : TargetableList) {
+      if (!ItTarget) continue;
+
+      // Check blacklist
+      if (FilterOut.Contains (ItTarget->GetOwner ())) continue;
+
+      // Check distance
+      if (FVector::Dist (ItTarget->GetComponentLocation (), Origin) > SearchDistance) continue;
+
+      // --- Check angle
+      FVector ItDir = ItTarget->GetComponentLocation () - Origin;
+      ItDir.Normalize ();
+      float ItAngle = (acosf (FVector::DotProduct (SearchDir, ItDir))) * (180 / 3.1415926);
+      if (ItAngle > SearchAngle) continue;
+
+      // First target
+      if (!CurrentTarget) {
+         CurrentTarget = ItTarget;
+         continue;
+      }
+
+      // --- Check if smaller angle
+      FVector CurrentDir = CurrentTarget->GetComponentLocation () - Origin;
+      CurrentDir.Normalize ();
+      float CurrentAngle = (acosf (FVector::DotProduct (SearchDir, CurrentDir))) * (180 / 3.1415926);
+
+      if (ItAngle < CurrentAngle) CurrentTarget = ItTarget;
+   }
+
+   return CurrentTarget;
+}
--- Source/RTargetable/RTargetableMgr.h
+++ Source/RTargetable/RTargetableMgr.h
@@ -17,14 +17,38 @@ public:
 
    URTargetableMgr ();
 
-   virtual void AddTarget (URTargetableComponent * Target);
-   virtual void RmTarget  (URTargetableComponent * Target);
-
+   //==========================================================================
+   //                  Params
+   //==========================================================================
+protected:
+   // Container for current targets
    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Rade|Targetable")
       TArray<URTargetableComponent*> TargetableList;
-
+public:
+   // Called when target list has been modified
    UPROPERTY(BlueprintAssignable, Category = "Rade|Targetable")
       FRTargetableEvent OnListUpdated;
+
+   // Distance at which target can be searched
+   UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Rade|Targetable")
+      float SearchDistance = 2000;
+
+   // FOV angle to search targets
+   UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Rade|Targetable")
+      float SearchAngle = 40;
+
+   //==========================================================================
+   //                  Functions
+   //==========================================================================
+
+   UFUNCTION(BlueprintCallable, Category = "Rade|Targetable")
+      virtual void AddTarget (URTargetableComponent * Target);
+
+   UFUNCTION(BlueprintCallable, Category = "Rade|Targetable")
+      virtual void RmTarget  (URTargetableComponent * Target);
+
+   UFUNCTION(BlueprintCallable, Category = "Rade|Targetable", meta = (AutoCreateRefTerm = "FilterOut"))
+      virtual URTargetableComponent* Find (FVector Origin, FRotator Direction, TArray<AActor*> FilterOut);
 
    //==========================================================================
    //                  Get instamce -> GameMode component
