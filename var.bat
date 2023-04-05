c2073a85442ce94e6b881718e08d6df1239e189e
Vagen <vagenv@gmail.com>
2023-04-05 10:54:44 -0700

    Added package script for Windows.

--- var.bat
+++ var.bat
@@ -12,4 +12,5 @@ set UE_EDITOR_EXE=%UE_DIR%/Engine/Binaries/Win64/UnrealEditor.exe
 set UE_EDITOR_CMD=%UE_DIR%/Engine/Binaries/Win64/UnrealEditor-cmd.exe
 set UE_BUILD_TOOL=%UE_DIR%/Engine/Binaries/DotNET/UnrealBuildTool/UnrealBuildTool.exe
 set UE_BUILD_BAT=%UE_DIR%/Engine/Build/BatchFiles/Build.bat
+set UE_BUILD_UAT=%UE_DIR%/Engine/Build/BatchFiles/RunUAT.bat
 
