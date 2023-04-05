09fba1567d1fb0adfff5f487224d562dfc8246a9
Vagen <vagenv@gmail.com>
2023-04-05 12:13:09 -0700

    Added blueprint build scripts.

---
+++ buildblueprint.bat
@@ -0,0 +1,5 @@
+@echo off
+
+call %~dp0\var.bat
+
+call %UE_EDITOR_CMD% %UPROJECT_PATH% -editortest -Execcmds="Automation SetFilter Stress, Automation list, Automation RunTests Project.Blueprints.Compile Blueprints" -testexit="Automation Test Queue Empty"
