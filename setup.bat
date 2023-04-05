bb2e580ce22eca06b73d5e4d83d2bdd3d28fc6e1
Vagen <vagenv@gmail.com>
2023-04-05 12:13:42 -0700

    Cleanup.

--- setup.bat
+++ setup.bat
@@ -3,5 +3,5 @@
 
 call %~dp0\var.bat
 
-%UE_BUILD_TOOL% -projectfiles -project=%UPROJECT_PATH% -game -rocket -progress
+call %UE_BUILD_TOOL% -projectfiles -project=%UPROJECT_PATH% -game -rocket -progress
 
