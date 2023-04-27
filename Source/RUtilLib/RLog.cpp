4aef604cc430152142e6de32e8f41a7aee4c1136
Vagen <vagenv@gmail.com>
2023-04-27 15:07:19 -0700

    RLOG. Fixed Invalid [Server/Client] prefix provided.

--- Source/RUtilLib/RLog.cpp
+++ Source/RUtilLib/RLog.cpp
@@ -5,9 +5,9 @@
 DEFINE_LOG_CATEGORY (RadeLog);
 
 FString RLog_GetAuthStr (const UObject* WorldContext) {
-   if (IsValid (WorldContext)) return "[STATIC]";
+   if (!IsValid (WorldContext)) return "[STATIC]";
    UWorld* World = WorldContext->GetWorld ();
-   if (!ensure (World))        return "[No World]";
+   if (!ensure (World))         return "[No World]";
    return World->IsNetMode (NM_Client) ? "[CLIENT] " : "[SERVER] ";
 }
 
