9b31d271fa9f1b7748cbbeede99ce8880265d7ae
Vagen <vagenv@gmail.com>
2023-06-06 14:14:01 -0700

    Will now generate both VSCode and Visual Studio project files.

--- setup.bat
+++ setup.bat
@@ -2,5 +2,6 @@
 
 call %~dp0\var.bat
 
-call %UE_BUILD_TOOL% -projectfiles -project=%UPROJECT_PATH% -game -rocket -progress
+call %UE_BUILD_TOOL% -projectfiles -project=%UPROJECT_PATH% -game -rocket -progress -VSCode
+call %UE_BUILD_TOOL% -projectfiles -project=%UPROJECT_PATH% -game -rocket -progress -2022
 
