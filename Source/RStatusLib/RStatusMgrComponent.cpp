29fca2297c1b90979c30f6ce2859be4ae7fd95d2
Vagen <vagenv@gmail.com>
2023-10-16 08:31:29 -0700

    RStatusLib. Only admin should recalculate status on BeginPlay.

--- Source/RStatusLib/RStatusMgrComponent.cpp
+++ Source/RStatusLib/RStatusMgrComponent.cpp
@@ -105,7 +105,7 @@ void URStatusMgrComponent::FindWorldMgrs ()
       return;
    }
 
-   RecalcStatus ();
+   if (R_IS_NET_ADMIN) RecalcStatus ();
 }
 
 //=============================================================================
