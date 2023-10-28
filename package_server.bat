eb09a4ae356a79d541c3e6f38141595863db3b0f
Vagen <vagenv@gmail.com>
2023-10-27 21:55:13 -0700

    Added dedicated server package sketch.

---
+++ package_server.bat
@@ -0,0 +1,40 @@
+@echo off
+
+call %~dp0\var.bat
+
+set PACKAGE_DIR=%PROJECT_DIR%/Build/%OS_TARGET%Server/
+
+call "%UE_BUILD_UAT%"                    ^
+   -ScriptsForProject="%UPROJECT_PATH%"  ^
+   Turnkey                               ^
+   -command=VerifySdk                    ^
+   -platform=%BUILD_PLATFORM%            ^
+   -UpdateIfNeeded                       ^
+   -EditorIO                             ^
+   -EditorIOPort=49968                   ^
+   -project="%UPROJECT_PATH%"            ^
+   BuildCookRun                          ^
+   -nop4                                 ^
+   -utf8output                           ^
+   -nocompileeditor                      ^
+   -skipbuildeditor                      ^
+   -cook                                 ^
+   -project="%UPROJECT_PATH%"            ^
+   -target=%PROJECT%Server               ^
+   -unrealexe="%UE_EDITOR_CMD%"          ^
+   -platform=%BUILD_PLATFORM%            ^
+   -installed                            ^
+   -stage                                ^
+   -archive                              ^
+   -package                              ^
+   -build                                ^
+   -pak                                  ^
+   -iostore                              ^
+   -compressed                           ^
+   -prereqs                              ^
+   -archivedirectory="%PACKAGE_DIR%"     ^
+   -CrashReporter                        ^
+   -server                               ^
+   -serverconfig="%BUILD_CONFIGURATION%" ^
+   -nocompile                            ^
+   -nocompileuat
