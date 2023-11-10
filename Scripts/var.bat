0129e2a8d6967486a144d955433030c401ac76c3
Vagen <vagenv@gmail.com>
2023-11-10 01:54:15 -0800

    Updated ROOTDIR path after scripts have been moved.

--- Scripts/var.bat
+++ Scripts/var.bat
@@ -1,8 +1,7 @@
 @echo off
 
-set ROOTDIR=%~dp0
-set ROOTDIR=%ROOTDIR:~0,-1%
-set UE_DIR=%~dp0/../UE/WINDOWS/UE_5.3
+set ROOTDIR=%~dp0/../
+set UE_DIR=%~dp0/../../UE/WINDOWS/UE_5.3
 
 set BUILD_PLATFORM=Win64
 set BUILD_CONFIGURATION=Development
