17955db709e8d97c994ce217bf99ea062c934c7e
Vagen <vagenv@gmail.com>
2023-12-22 18:36:36 -0800

    Fixed typo in clean.bat script.

--- Scripts/clean.bat
+++ Scripts/clean.bat
@@ -5,5 +5,5 @@ call %~dp0\var.bat
 rd /s /q %PROJECT_DIR%\Intermediate\
 rd /s /q %PROJECT_DIR%\Binaries\
 
-call %UE_BUILD_BAT% %PROJECT%Editor %BUILD_PLATFORM% %BUILD_CONFIGURATION% "%UPROJECT_PATH%" -waitmutex -Clean
+call %UE_BUILD_BAT% %PROJECT_NAME%Editor %BUILD_PLATFORM% %BUILD_CONFIGURATION% "%UPROJECT_PATH%" -waitmutex -Clean
 
